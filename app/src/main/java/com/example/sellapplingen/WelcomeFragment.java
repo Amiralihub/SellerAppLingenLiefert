037acd129894846d9b3597fc314e4f20514f630e
raniamo8 <raneaxxl@gmail.com>
2023-07-17 23:52:56 +0200

    log in fragment

--- app/src/main/java/com/example/sellapplingen/WelcomeFragment.java
+++ app/src/main/java/com/example/sellapplingen/WelcomeFragment.java
@@ -40,7 +40,7 @@ public class WelcomeFragment extends Fragment {
         loginButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                Intent intent = new Intent(getContext(), LoginActivity.class);
                 startActivity(intent);
             }
         });
