e9bebccdfde4fdc4ed5552e16b40baa153ec336c
raniamo8 <raneaxxl@gmail.com>
2023-07-25 00:48:04 +0200

    TODO's

--- app/src/main/java/com/example/sellapplingen/SettingFragment.java
+++ app/src/main/java/com/example/sellapplingen/SettingFragment.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-//TODO XML (Alaa): Die Ansicht wie iOS realisieren
+//TODO XML (Alaa): Die Ansicht in der Datei fragment_setting.xml wie iOS realisieren
 
 public class SettingFragment extends Fragment {
 
