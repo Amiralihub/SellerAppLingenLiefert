2715b3b233fb7d93755cc5138b595c1e0464d442
raniamo8 <raneaxxl@gmail.com>
2023-07-25 20:58:07 +0200

    TODO's

---
+++ app/src/main/java/com/example/sellapplingen/Order.java
@@ -0,0 +1,9 @@
+package com.example.sellapplingen;
+
+//TODO Alaa: Intialisiere die Klasse Order basierend auf die App von Jan
+//TODO Alaa: Bau die Getter und Setter auf
+//TODO Alaa: Erstelle einen leeren und einen vollen Konstruktor
+//TODO Alaa: Erstelle eine toString() Methode
+
+public class Order {
+}
