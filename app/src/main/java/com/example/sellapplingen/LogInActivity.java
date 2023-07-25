1152e966904618005c66ade6b9bf90267557f2c8
raniamo8 <raneaxxl@gmail.com>
2023-07-25 20:45:59 +0200

    clean up

---
+++ app/src/main/java/com/example/sellapplingen/LogInActivity.java
@@ -0,0 +1,23 @@
+package com.example.sellapplingen;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.example.sellapplingen.databinding.ActivityLogInBinding;
+
+//login erstelln
+//Logik über login klasse
+
+public class LogInActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityLogInBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
