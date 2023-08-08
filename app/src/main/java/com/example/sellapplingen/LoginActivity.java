5ad34655be627a87a918d169bd5aea32be3534b8
FrecherFruchtzwerg <sadik-bajrami@gmx.de>
2023-08-08 19:36:57 +0200

    Order klasse anzeigen in deliverydetails

--- app/src/main/java/com/example/sellapplingen/LoginActivity.java
+++ app/src/main/java/com/example/sellapplingen/LoginActivity.java
@@ -27,16 +27,43 @@ public class LoginActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.editTextPassword);
         Button loginButton = findViewById(R.id.buttonLogin);
 
-        loginButton.setOnClickListener(v -> startMainActivity());
-    }
-    private void startMainActivity() {
-        // Starte die MainActivity
-        startActivity(new Intent(this, MainActivity.class));
-        finish(); // Beende die LoginActivity
+        loginButton.setOnClickListener(v -> login());
     }
 
 
 
 
+    private void login() {
+        String enteredUsername = usernameEditText.getText().toString();
+        String enteredPassword = passwordEditText.getText().toString();
 
+        try {
+            // Sende die Benutzerdaten an den Server
+            loginManager.sendPost();
+        } catch (NullPointerException e) {
+            // Handle NullPointerException
+            Toast.makeText(this, "Name or password is null", Toast.LENGTH_SHORT).show();
+            return;
+        } catch (IllegalArgumentException e) {
+            // Handle IllegalArgumentException
+            Toast.makeText(this, "Name or password is empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Überprüfe die Login-Daten mit dem LoginManager
+        if (loginManager.isLoginValid(enteredUsername, enteredPassword)) {
+            // Erfolgreich eingeloggt - hier kannst du die Hauptactivity starten oder andere Aktionen ausführen
+            Toast.makeText(this, "Login erfolgreich!", Toast.LENGTH_SHORT).show();
+
+            // Speichere die Login-Daten im LoginManager
+            loginManager.saveLoginCredentials(enteredUsername, enteredPassword);
+
+            // Starte die MainActivity
+            startActivity(new Intent(this, MainActivity.class));
+            finish(); // Beende die LoginActivity
+        } else {
+            // Falsche Anmeldedaten
+            Toast.makeText(this, "Ungültiger Benutzername oder Passwort.", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
