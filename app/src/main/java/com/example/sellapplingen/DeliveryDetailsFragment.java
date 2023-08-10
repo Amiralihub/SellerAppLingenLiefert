07664a7a08d84aada5e23f03db9b3b372cb86397
FrecherFruchtzwerg <sadik-bajrami@gmx.de>
2023-08-11 00:04:58 +0200

    Order Funktion

--- app/src/main/java/com/example/sellapplingen/DeliveryDetailsFragment.java
+++ app/src/main/java/com/example/sellapplingen/DeliveryDetailsFragment.java
@@ -3,9 +3,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class DeliveryDetailsFragment extends Fragment {
 
@@ -33,6 +36,19 @@ public class DeliveryDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_delivery_details, container, false);
+
+        Button backToFragmentButton = view.findViewById(R.id.backToFragmentButton);
+        backToFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HandlingInfoFragment handlingInfoFragment = new HandlingInfoFragment();
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.frame_layout, handlingInfoFragment);
+                transaction.commit();
+            }
+        });
+
 
         // Hole das Order-Objekt aus den Fragment-Argumenten
         Bundle args = getArguments();
@@ -68,6 +84,12 @@ public class DeliveryDetailsFragment extends Fragment {
             String deliveryAddress = order.getStreet() + " " + order.getHouseNumber() + ", " +
                     order.getZip() + " " + order.getCity();
             deliveryAddressValue.setText(deliveryAddress);
+
+            TextView cityLabel = view.findViewById(R.id.cityValue);
+            cityLabel.setText(order.getCity());
+
+            TextView zipLabel = view.findViewById(R.id.zipValue);
+            zipLabel.setText(order.getZip());
 
             // Weitere TextViews für andere Order-Informationen hinzufügen
             // Hier kannst du weitere TextViews hinzufügen, um andere Order-Informationen anzuzeigen
